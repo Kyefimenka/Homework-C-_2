9977e3fab40589ddb4e7102a89a516fd43265f68
Katsiaryna <caelpi85@gmail.com>
2022-06-29 17:46:12 +0200

    Task about second digit of three-digit number

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Enter three-digit number");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// int secondDigit = threeDigitNumber / 10 % 10;
+int secondDigit = threeDigitNumber % 100 / 10;
+Console.WriteLine($"Second number is: {secondDigit}");

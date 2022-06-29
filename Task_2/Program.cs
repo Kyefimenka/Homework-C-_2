b84c8763a2e728db90681ee7167d810e64a10970
Katsiaryna <caelpi85@gmail.com>
2022-06-29 19:10:43 +0200

    Task about third digit output

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter number");
+int curentNumber = Convert.ToInt32(Console.ReadLine());
+
+if (curentNumber / 100 == 0)
+{
+    Console.WriteLine("Third digit not available");
+}
+else
+{
+    while (curentNumber / 1000 != 0)
+    {
+        curentNumber /= 10;
+    }
+    Console.WriteLine(curentNumber % 10);
+}
+

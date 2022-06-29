d31172fb18783e3af9e018e37dd65601dbd09723
Katsiaryna <caelpi85@gmail.com>
2022-06-29 19:52:54 +0200

    Task about day of the week

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.n
+
+int digit = 0;
+while(digit < 1 || digit > 7)
+{
+    Console.WriteLine("Enter digit in [1..7]");
+    digit = Convert.ToInt32(Console.ReadLine());
+}
+if(digit >= 5)
+{
+    Console.WriteLine("This is a weekday");
+}
+else
+{
+    Console.WriteLine("This is the weekend");
+}
+

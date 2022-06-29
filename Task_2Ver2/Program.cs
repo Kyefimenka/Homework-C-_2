960dad9d750e065c1951b845750d68fc336168b6
Katsiaryna <caelpi85@gmail.com>
2022-06-29 21:09:54 +0200

    Second solution for task 2

---
+++ Task_2Ver2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter number");
+string strNumber = Console.ReadLine();
+
+if(strNumber.Length < 3 || '0' > strNumber[2] || strNumber[2] > '9')
+{
+    Console.WriteLine("Third digit not available");
+}
+else
+{
+    Console.WriteLine(strNumber[2]);
+}
+
+
+
+
+
